c6fa8dbc444c7a17a633a8ae1eb4cfdba7f4aea5
Geison Jair <galcantaras1@upao.edu.pe>
2025-09-02 19:31:39 -0500

    Version 2

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,5 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://lcnmlrghlhdqnskxtkjr.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxjbm1scmdobGhkcW5za3h0a2pyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NTU5NTAsImV4cCI6MjA3MDMzMTk1MH0.AF2aTgrFtlG-CJOtbA7jhycBdbg2k55VJaOEV5de2Rk";
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// Lee las variables de entorno de forma segura
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
